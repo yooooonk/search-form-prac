79c1543ad1f606ad5f3d1da5639801ef58c99c3f
ouo_yoonk <joyk9251@gmail.com>
2020-10-31 11:51:22 +0900

    keyup 이벤트추가

--- my-vanilla/js/views/FormView.js
+++ my-vanilla/js/views/FormView.js
@@ -9,10 +9,20 @@ FormView.setUp = function(el){
     this.inputEl = el.querySelector('[type=text]')
     this.resetEl = el.querySelector('[type=reset]')
     this.showResetBtn(false)
+    this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function(show = true){
     this.resetEl.style.display = show? 'block':'none'
+}
+
+FormView.bindEvents = function(){
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+}
+
+FormView.onKeyup() = function(e){
+    this.showResetBtn(this.inputEl.value.length)
 }
 
 export default FormView
