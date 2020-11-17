5fa249c68d05a0970eccee5ddf6ff0260f519a78
ouo_yoonk <joyk9251@gmail.com>
2020-11-17 21:08:23 +0900

    [요구사항] 뷰 검색 결과 구현 [상세기능] 검색 겨로가가 검색폼 아래 위치한다 검색 결과가 보인다 x 버튼을 클릭하면 검색폼이 초기화 되고, 검색 결과가 사라진다

--- my-vue/js/app.js
+++ my-vue/js/app.js
@@ -1,11 +1,16 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el:'#app', // 뷰 인스턴스가 어느 부분에 마운트될것인지 설정
     data:{
-        query : ''
+        query : '',
+        submitted : false,
+        searchResult : ''
+        
     },
     methods:{ // DOM과 바인딩한 함수 정의
         onSubmit(e){
-            
+            this.search()
         },
         onKeyup(){
             if(!this.query.legnth){
@@ -13,7 +18,19 @@ new Vue({
             }
         },
         onClickResetBtn(){
+            this.resetForm()
+        },        
+        resetForm(){
             this.query = ''
+            this.searchResult=''
+            this.submitted=false
+        },
+        search(){
+            this.submitted = true;                        
+            SearchModel.list().then(data=>{
+                
+                this.searchResult = data
+            })
         }
     }
 })
